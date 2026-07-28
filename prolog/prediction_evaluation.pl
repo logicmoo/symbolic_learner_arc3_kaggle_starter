42bbca1457aa8269f281de3bfc28dde4f39ab5ed
Douglas R. Miles <logicmoo@gmail.com>
2026-07-29 06:26:00 +0800

    Add Prolog prediction evaluation contract

---
+++ prolog/prediction_evaluation.pl
@@ -0,0 +1,30 @@
+:- module(prediction_evaluation, [
+    evaluate_prediction/4,
+    grade_recorded_prediction/6
+]).
+
+:- use_module(prediction_ledger).
+
+evaluate_prediction(Predicted, Observed, Comparator, Grade) :-
+    must_be(callable, Comparator),
+    call(Comparator, Predicted, Observed, Grade),
+    must_be(number, Grade).
+
+grade_recorded_prediction(
+    PredictionId,
+    OutcomeSequence,
+    Observed,
+    Comparator,
+    Grade,
+    Closed
+) :-
+    prediction_ledger:prediction_record(PredictionId, Prediction),
+    Predicted = Prediction.predicted_effects,
+    evaluate_prediction(Predicted, Observed, Comparator, Grade),
+    prediction_ledger:grade_prediction(
+        PredictionId,
+        OutcomeSequence,
+        Observed,
+        Grade,
+        Closed
+    ).
