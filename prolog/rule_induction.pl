4bc91f109137a94b829f9e7a81edf15eb99b3fb9
Douglas R. Miles <logicmoo@gmail.com>
2026-07-29 06:40:58 +0800

    Preserve rule induction callback module context

--- prolog/rule_induction.pl
+++ prolog/rule_induction.pl
@@ -4,6 +4,10 @@
     generalize_rule/4
 ]).
 
+:- meta_predicate propose_transition_rules(+, +, 3, -).
+:- meta_predicate specialize_rule(+, +, 3, -).
+:- meta_predicate generalize_rule(+, +, 3, -).
+
 propose_transition_rules(Candidates, Context, Inducer, Rules) :-
     must_be(callable, Inducer),
     call(Inducer, Candidates, Context, Rules).
