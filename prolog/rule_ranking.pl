1097673f4ab60f9fc6bfe3f225baa119ff95756d
Douglas R. Miles <logicmoo@gmail.com>
2026-07-29 06:25:51 +0800

    Add Prolog rule ranking contract

---
+++ prolog/rule_ranking.pl
@@ -0,0 +1,21 @@
+:- module(rule_ranking, [
+    rank_rules/3,
+    preferred_rule/2
+]).
+
+:- use_module(library(pairs)).
+
+rank_rules(Rules, Scorer, Ranked) :-
+    must_be(callable, Scorer),
+    maplist(score_rule(Scorer), Rules, Scored),
+    keysort(Scored, Ascending),
+    reverse(Ascending, Descending),
+    maplist(to_ranked, Descending, Ranked).
+
+score_rule(Scorer, Rule, Score-Rule) :-
+    call(Scorer, Rule, Score),
+    must_be(number, Score).
+
+to_ranked(Score-Rule, ranked(Score, Rule)).
+
+preferred_rule([ranked(_, Rule)|_], Rule).
