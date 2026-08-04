b62d29178882835a29f5fbdd0e4cbf6f8586274e
Douglas R. Miles <logicmoo@gmail.com>
2026-08-04 18:35:17 +0800

    Install uppercase-G LLM batch profile UI

--- scripts/interactive_runner.py
+++ scripts/interactive_runner.py
@@ -9,9 +9,11 @@ from llm_json_patch import install_llm_json_resilience
 install_llm_json_resilience()
 
 import interactive_runner as interactive_runner_ui
+from llm_batch_profiles import install_batch_ui
 from multillm_runner import install_interactive_runner
 
 install_interactive_runner(interactive_runner_ui)
+install_batch_ui(interactive_runner_ui)
 
 
 if __name__ == "__main__":
