a79d843d0adee2c83e4efd8cfc48d982b42b3c89
Douglas R. Miles <logicmoo@gmail.com>
2026-08-07 04:27:48 +0800

    Load model and task configurator styles

--- workbench/frontend/src/main.tsx
+++ workbench/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/workbench.css";
 import "./styles/workspace_backed.css";
+import "./styles/llm_models.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
